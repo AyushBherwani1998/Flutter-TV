7537ec550b345eb20d849e328f6cad46fca9adde
AyushBherwani1998 <34301187+AyushBherwani1998@users.noreply.github.com>
2018-12-13 15:26:16 +0530

    updated

--- fluter_trivia/lib/movieUserInterface.dart
+++ fluter_trivia/lib/movieUserInterface.dart
@@ -14,6 +14,12 @@ class MyMovieUserInterface extends State<MovieUserInterface> {
     size: 20.0,
   );
 
+  Icon _starIconR = new Icon(
+    Icons.star_border,
+    color: Colors.white,
+    size: 20.0,
+  );
+
   void _starIconPressed() {
     setState(() {
       if (this._starIcon.icon == Icons.star_border) {
@@ -32,11 +38,31 @@ class MyMovieUserInterface extends State<MovieUserInterface> {
     });
   }
 
+  void _starIconPressedR() {
+    setState(() {
+      if (this._starIconR.icon == Icons.star_border) {
+        this._starIconR = new Icon(
+          Icons.star,
+          color: Colors.yellow.shade500,
+          size: 20.0,
+        );
+      } else {
+        this._starIconR = new Icon(
+          Icons.star_border,
+          color: Colors.white,
+          size: 20.0,
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Container(
       color: Colors.grey.shade800,
-      child: Column(
+      child: ListView(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
         children: <Widget>[
           new Container(
             height: 20.0,
@@ -151,10 +177,11 @@ class MyMovieUserInterface extends State<MovieUserInterface> {
               ],
             ),
           ),
+          Divider(color: Colors.grey,indent: 8.0,),
           new Container(
             height: 20.0,
             width: MediaQuery.of(context).size.width,
-            margin: const EdgeInsets.only(top: 16.0, left: 8.0),
+            margin: const EdgeInsets.only(left: 8.0),
             child: new Text(
               "Recommended",
               textDirection: TextDirection.ltr,
@@ -162,13 +189,112 @@ class MyMovieUserInterface extends State<MovieUserInterface> {
                   color: Colors.white, fontSize: 14.0, fontFamily: 'Oswald'),
             ),
           ),
-
           new Container(
             height: 120.0,
-            margin: EdgeInsets.only(left: 8.0,right: 8.0,top: 8.0),
-          )
-        ],
+            margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+            child: new ListView(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                new Stack(
+                  children: <Widget>[
+                    Image.asset(
+                      "images/simmba_poster.jpg",
+                      width: 180.0,
+                      height: 120.0,
+                      fit: BoxFit.fill,
+                    ),
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          height: 20.0,
+                          width: 180.0,
+                          margin: EdgeInsets.only(top: 100.0),
+                          color: Colors.white.withOpacity(0.4),
+                        ),
+                        GestureDetector(
+                          onTap: _starIconPressedR,
+                          child: Container(
+                            padding: EdgeInsets.all(1.0),
+                            margin: EdgeInsets.only(left: 156.0, top: 98.0),
+                            child: _starIconR,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                new Padding(padding: EdgeInsets.only(left: 8.0)),
+                new Stack(
+                  children: <Widget>[
+                    Image.asset(
+                      "images/gold_poster.jpg",
+                      width: 180.0,
+                      height: 120.0,
+                      fit: BoxFit.fill,
+                    ),
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          height: 20.0,
+                          width: 180.0,
+                          margin: EdgeInsets.only(top: 100.0),
+                          color: Colors.white.withOpacity(0.4),
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.all(1.0),
+                            margin: EdgeInsets.only(left: 156.0, top: 99.0),
+                            child: Icon(
+                              Icons.star_border,
+                              color: Colors.white,
+                              size: 20.0,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                new Padding(padding: EdgeInsets.only(left: 8.0)),
+                new Stack(
+                  children: <Widget>[
+                    Image.asset(
+                      "images/Padman_poster.jpg",
+                      width: 180.0,
+                      height: 120.0,
+                      fit: BoxFit.fill,
+                    ),
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          height: 20.0,
+                          width: 180.0,
+                          margin: EdgeInsets.only(top: 100.0),
+                          color: Colors.white.withOpacity(0.4),
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.all(1.0),
+                            margin: EdgeInsets.only(left: 156.0, top: 99.0),
+                            child: Icon(
+                              Icons.star_border,
+                              color: Colors.white,
+                              size: 20.0,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
 
+          Divider(color: Colors.grey,indent: 8.0,),
+          Container()
+        ],
       ),
     );
   }
